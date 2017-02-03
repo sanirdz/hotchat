3c26330fd806c51f2826e3a1f6330dac5c640e7b
paulo <paulohsr@hotmail.com>
2017-02-03 11:47:13 -0200

    Novos testes

--- src/test/java/br/com/paulo/hotchat/service/ContatoServiceTest.java
+++ src/test/java/br/com/paulo/hotchat/service/ContatoServiceTest.java
@@ -1,5 +1,84 @@
 package br.com.paulo.hotchat.service;
 
-public class ContatoServiceTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+
+import br.com.paulo.hotchat.domain.Contato;
+import br.com.paulo.hotchat.domain.Usuario;
+import br.com.paulo.hotchat.repository.ContatoRepository;
+import br.com.paulo.hotchat.repository.UsuarioRepository;
+import br.com.paulo.hotchat.websocket.UsuariosConectados;
+
+public class ContatoServiceTest {
+	
+	@Mock
+	private UsuarioRepository usuarioRepositoryMock;
+	
+	@Mock
+	private UsuariosConectados usuariosConectadosMock;
+	
+	@Mock
+	private ContatoRepository contatoRepositoryMock;
+	
+	@Mock
+	private MensagemService mensagemServiceMock;
+
+	private ContatoService contatoService;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		contatoService = new ContatoService(usuarioRepositoryMock, usuariosConectadosMock, contatoRepositoryMock, mensagemServiceMock);
+		
+		when(usuarioRepositoryMock.findByLogin(anyString())).thenAnswer((InvocationOnMock i) -> {
+			return new Usuario().setLogin(i.getArgumentAt(0, String.class));
+		});
+	}
+	
+	@Test
+	public void listarContatosUsuarioExistenteRetornaListaComUmOnlineUmOffline() {
+		Usuario usuario = new Usuario().setLogin("paulo");
+		List<Contato> contatos = new ArrayList<>();
+		contatos.add(new Contato().setContato(new Usuario().setLogin("contato1")));
+		contatos.add(new Contato().setContato(new Usuario().setLogin("contato2")));
+		
+		when(contatoRepositoryMock.findAllByPrincipal(usuario)).thenReturn(contatos);
+		when(usuariosConectadosMock.estaConectado("contato1")).thenReturn(true);
+		when(usuariosConectadosMock.estaConectado("contato2")).thenReturn(false);
+		
+		List<Contato> lista = (List<Contato>) contatoService.listarContatos("paulo");
+		
+		assertThat(lista).hasSize(2);
+		
+		assertThat(lista.get(0).getContato().getLogin()).isEqualTo("contato1");
+		assertThat(lista.get(0).getContato().getOnline()).isTrue();
+		
+		assertThat(lista.get(1).getContato().getLogin()).isEqualTo("contato2");
+		assertThat(lista.get(1).getContato().getOnline()).isFalse();
+	}
+
+	@Test
+	public void salvarContatoRetornaContatoOnlineSalvo() {
+		when(usuariosConectadosMock.estaConectado(anyString())).thenReturn(true);
+		when(contatoRepositoryMock.save(any(Contato.class))).thenAnswer((InvocationOnMock i) -> {
+			return i.getArgumentAt(0, Contato.class).setId(1L);
+		});
+		
+		Contato contato = contatoService.salvarContato("login", "contato");
+		
+		assertThat(contato.getContato().getOnline()).isTrue();
+		assertThat(contato.getPrincipal().getLogin()).isEqualTo("login");
+		assertThat(contato.getContato().getLogin()).isEqualTo("contato");
+	}
 }
--- src/test/java/br/com/paulo/hotchat/service/MensagemServiceTest.java
+++ src/test/java/br/com/paulo/hotchat/service/MensagemServiceTest.java
@@ -1,5 +1,115 @@
 package br.com.paulo.hotchat.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+
+import br.com.paulo.hotchat.domain.Contato;
+import br.com.paulo.hotchat.domain.Mensagem;
+import br.com.paulo.hotchat.domain.Usuario;
+import br.com.paulo.hotchat.repository.MensagemRepository;
+import br.com.paulo.hotchat.repository.UsuarioRepository;
+import br.com.paulo.hotchat.websocket.UsuariosConectados;
+import br.com.paulo.hotchat.websocket.WebSocketService;
+
 public class MensagemServiceTest {
 
+	@Mock
+	private UsuarioRepository usuarioRepositoryMock;
+	@Mock
+	private MensagemRepository mensagemRepositoryMock;
+	@Mock
+	private WebSocketService webSocketServiceMock;
+	@Mock
+	private UsuariosConectados usuariosConectadosMock;
+	@Mock
+	private ContatoService contatoServiceMock;
+	
+	private MensagemService mensagemService;
+	
+	private String loginEmissor = "emissor";
+	private String loginDestinatario = "destinatario";
+	
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		mensagemService = new MensagemService(usuarioRepositoryMock, mensagemRepositoryMock, webSocketServiceMock, usuariosConectadosMock);
+		mensagemService.setContatoService(contatoServiceMock);
+	
+		
+		when(contatoServiceMock.recuperaContato(loginEmissor, loginDestinatario)).then((InvocationOnMock i) -> {
+			return new Contato()
+					.setContato(new Usuario().setLogin(i.getArgumentAt(1, String.class)))
+					.setPrincipal(new Usuario().setLogin(i.getArgumentAt(0, String.class)));
+		});
+		
+		when(usuarioRepositoryMock.findByLogin(anyString())).then((InvocationOnMock i) -> {
+			return new Usuario().setLogin(i.getArgumentAt(0, String.class));
+		});
+		
+		List<Mensagem> mensagensEnviadas = new ArrayList<>();
+		mensagensEnviadas.add(new Mensagem().setConteudo("mensagem 3").setDataEnvio(LocalDateTime.of(2010, 1, 1, 1, 3)));
+		mensagensEnviadas.add(new Mensagem().setConteudo("mensagem 1").setDataEnvio(LocalDateTime.of(2010, 1, 1, 1, 1)));
+		
+		List<Mensagem> mensagensRecebidas = new ArrayList<>();
+		mensagensRecebidas.add(new Mensagem().setConteudo("mensagem 4").setDataEnvio(LocalDateTime.of(2010, 1, 1, 1, 4)));
+		mensagensRecebidas.add(new Mensagem().setConteudo("mensagem 2").setDataEnvio(LocalDateTime.of(2010, 1, 1, 1, 2)));
+		when(mensagemRepositoryMock.findAllByDestinatarioAndEmissorOrderByDataEnvio(any(Usuario.class), any(Usuario.class))).thenReturn(mensagensEnviadas, mensagensRecebidas);
+	}
+	
+	@Test
+	public void listarMensagensDestinatarioEmissor() {
+		TreeSet<Mensagem> lista = (TreeSet<Mensagem>) mensagemService.listarMensagensDestinatarioEmissor(loginDestinatario, loginEmissor);
+		
+		assertThat(lista).hasSize(4);
+		assertThat(lista.pollFirst().getConteudo()).isEqualTo("mensagem 1");
+		assertThat(lista.pollFirst().getConteudo()).isEqualTo("mensagem 2");
+		assertThat(lista.pollFirst().getConteudo()).isEqualTo("mensagem 3");
+		assertThat(lista.pollFirst().getConteudo()).isEqualTo("mensagem 4");
+	}
+	
+	@Test
+	public void listarMensagensDestinatarioEmissorContatoBloqueadoRetornaApenasMensagensEnviadas() {
+		when(contatoServiceMock.recuperaContato(loginEmissor, loginDestinatario)).then((InvocationOnMock i) -> {
+			return new Contato()
+					.setContato(new Usuario().setLogin(i.getArgumentAt(1, String.class)))
+					.setPrincipal(new Usuario().setLogin(i.getArgumentAt(0, String.class)))
+					.setBloqueado(true);
+		});
+		
+		TreeSet<Mensagem> lista = (TreeSet<Mensagem>) mensagemService.listarMensagensDestinatarioEmissor(loginDestinatario, loginEmissor);
+		
+		assertThat(lista).hasSize(2);
+		assertThat(lista.pollFirst().getConteudo()).isEqualTo("mensagem 1");
+		assertThat(lista.pollFirst().getConteudo()).isEqualTo("mensagem 3");
+	}
+
+	public void marcarMensagensLidas() {
+	
+	}
+	
+	public void recuperaQuantidadeMensagensNaoLidas() {
+	
+	}
+
+	public void enviarMensagem() {
+	
+	}
+
+	public void marcarMensagemLida() {
+	
+	}
 }
